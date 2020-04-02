3207b1a737afc1f1df8b93b9be07750d86cf8fbb
xuanmos <me@xuanmo.xin>
2020-04-02 09:25:51 +0800

    增加自定义校验规则功能

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import Base from './Base/index.vue'
+import { extend } from 'vee-validate'
 
 const VForm = {
   install (vue, opt = {}) {
     Vue.component('VForm', Base)
     Vue.prototype.$VForm = opt
+
+    // 注册自定义校验规则
+    for (const [key, val] of Object.entries(opt.validator)) {
+      extend(key, val)
+    }
   }
 }
+
+// 对外暴露自定义校验规则方法
+export { extend }
 
 export default VForm
