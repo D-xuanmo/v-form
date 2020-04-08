9258360f43ae1e1033486f846a16940d2f0b2cb6
xuanmos <me@xuanmo.xin>
2020-04-08 23:28:14 +0800

    fix bug

--- src/Text/index.vue
+++ src/Text/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="v-form-text" @click="__eventHandler('click')">
+  <div class="v-form-text" @click="__eventHandler('click', formModel)">
     <span v-if="!formModel.value && formModel.rules.placeholder" class="placeholder">{{ formModel.rules.placeholder }}</span>
     <template v-else>{{ formModel.value }}</template>
   </div>
