48c157cdce378576efe4d24fa04d6eb6445fc3ef
xuanmos <me@xuanmo.xin>
2020-04-08 21:37:34 +0800

    配置文件更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint',
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-    extends: ['plugin:vue/recommended', 'eslint:recommended'],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent: ['warn', 2],
-        'vue/html-indent': ['warn', 2],
-        'no-param-reassign': 'off',
-        'max-len': ['error', 150, 2],
-        semi: 'off',
-        'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
-        'vue/max-attributes-per-line': 'off',
-        'vue/singleline-html-element-content-newline': 'off',
-        'vue/html-closing-bracket-spacing': 'off',
-        'vue/html-self-closing': 'off',
-        'vue/multiline-html-element-content-newline': 'off',
-        'vue/mustache-interpolation-spacing': 'off'
-    }
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: ['warn', 2],
+    'vue/html-indent': ['warn', 2],
+    'no-param-reassign': 'off',
+    'max-len': ['error', 150, 2],
+    semi: 'off',
+    'no-unused-vars': ['error', {
+      vars: 'all',
+      args: 'none',
+      ignoreRestSiblings: true
+    }],
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/mustache-interpolation-spacing': 'off'
+  }
 };
