782aa26ec0ff95217063b3d02ec0db9f535dbd45
xuanmos <me@xuanmo.xin>
2020-05-15 10:18:37 +0800

    fix bug

--- src/Base/index.vue
+++ src/Base/index.vue
@@ -8,10 +8,7 @@
           :label="v.rules.label"
           :required="(v.rules.vRules || '').indexOf('required') !== -1"
         >
-          <slot slot="extra" :name="`${key}-extra`">
-            <!-- eslint-disable-next-line -->
-            <div v-html="v.rules.extra"></div>
-          </slot>
+          <slot slot="extra" :name="`${key}-extra`"></slot>
           <slot :name="key" :data="v">
             <component
               :is="_splitComponentType(v.rules.type)[0]"
